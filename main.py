08760cdd7e8c3d191ac2949285f6197bb332a261
inspflof <155272834+inspflof@users.noreply.github.com>
2025-12-10 11:24:13 +0100

    add serpent class and edit linkedlist class to fit with serpent class

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ class Node:
         self.data = data
 
 class LinkedList:
-    def __init__(self, nodeClass):
-        self.head = None
+    def __init__(self, nodeClass, initData=(0,0)):
+        self.head = nodeClass(initData)
         self.node = nodeClass
 
     def insertAtEnd(self, data):
         new_node = self.node(data)  # Create a new node
-        if self.head is None:
-            self.head = new_node  # If the list is empty, make the new node the head
-            return
         last = self.head 
-        while last.next:  # Otherwise, traverse the list to find the last node
+        while last.next:  # traverse the list to find the last node
             last = last.next
         last.next = new_node  # Make the new node the next node of the last node
 
@@ -26,3 +23,19 @@ class LinkedList:
             print(temp.data,end=' ') # Print the data in the current node
             temp = temp.next # Move to the next node
         print()  # Ensures the output is followed by a new line
+
+class Serpent:
+    def __init__(self, bodyClass, nodeClass):
+        self.bodyClass = bodyClass
+        self.nodeClass = nodeClass
+        self.body = self.bodyClass(self.nodeClass)
+
+    def createSegment(self, coordonnees):
+        self.body.insertAtEnd(coordonnees)
+
+    def test(self):
+        self.body.printList()
+
+snake = Serpent(LinkedList, Node)
+snake.createSegment((0,1))
+snake.test()
